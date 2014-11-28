9b366b67207154b39fa8d427df1b818d09bdfe82
James Gatannah <james.gatannah@gmail.com>
2014-11-28 10:19:38 -0600

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -9,10 +9,10 @@
                  [im.chit/ribol "0.4.0"]
                  [mvxcvi/puget "0.6.4"]
                  [org.clojure/clojure "1.6.0"]
-                 [prismatic/schema "0.3.0"]]
+                 [prismatic/schema "0.3.3"]]
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.6"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.7"]
                                   [org.clojure/java.classpath "0.2.2"]]}
              :uberjar {:aot :all}}
   :repl-options {:init-ns user})
