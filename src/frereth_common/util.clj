1a976b43bb386f26595c6790dfeb455bbe9e3006
James Gatannah <james.gatannah@gmail.com>
2014-12-21 22:06:45 -0600

    Start implementing reflection
    This is a horribly broken check-in, but I have to do what I can manage
    in a super-limited time frame right now

--- src/frereth_common/util.clj
+++ src/frereth_common/util.clj
@@ -1,5 +1,10 @@
 (ns frereth-common.util
-  (:require [puget.printer :as puget]))
+  (:require [puget.printer :as puget]
+            [ribol.core :refer (raise)]
+            [schema.core :as s]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; TODO
 
 ;; Global functions like this are bad.
 ;; Especially since I'm looking at a
@@ -9,6 +14,35 @@
 ;; instead
 (puget/set-color-scheme! :keyword [:bold :green])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Internal
+
+(defn describe-annotations
+  [as]
+  (raise :not-implemented))
+
+(s/defn interpret-modifiers :- s/Keyword
+  [ms :- s/Int]
+  (raise :not-implemented))
+
+(defn describe-field
+  [f]
+  {:name (.getName f)
+   :annotations (map describe-annotations (.getDeclaredAnnotations f))
+   :modifiers (interpret-modifiers (.getModifiers f))})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public
+
 (defn pretty
   [& os]
   (with-out-str (apply puget/cprint os)))
+
+(defn dir
+  [something]
+  (let [k (class something)
+        fields (.getDeclaredFields k)
+        useful-fields (map describe-field fields)
+        methods (.getDeclaredMethods k)]
+    {:fields useful-fields}
+    (raise :not-implemented)))
