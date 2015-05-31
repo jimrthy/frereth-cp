ef239da1161c4e6fc4c588a421ac8dc59959cf3a
James Gatannah <jgatannah@gmail.com>
2015-05-31 22:33:07 +0000

    Move common library dependencies here
    Because trying to keep them up to date in 4 separate projects is a
    losing proposition. Everything that uses these dependencies can get the
    "proper" one transitively through here.
    
    Also added a dependency to my new component-dsl.

--- project.clj
+++ project.clj
@@ -1,18 +1,39 @@
-(defproject frereth-common "0.0.1-SNAPSHOT"
+(defproject com.frereth/common "0.0.1-SNAPSHOT"
   :description "Pieces that the different Frereth parts share"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;; TODO: Could I totally pull datomic dependencies out of everything else?
-  :dependencies [[com.stuartsierra/component "0.2.3"]
-                 [com.taoensso/timbre "3.4.0" :exclusions [org.clojure/tools.reader]]
+  :dependencies [;; Q: Does this make any sense in production?
+                 ;; A: Well, it makes sense for the general runtime which
+                 ;; is the primary goal.
+                 [com.cemerick/pomegranate "0.3.0" :exclusions [org.codehaus.plexus/plexus-utils]]
+                 ;; For now, this next library needs to be distributed to
+                 ;; a local maven repo.
+                 ;; It seems like it should really take care of its handler
+                 ;; ...except that very likely means native libraries, so
+                 ;; it gets more complicated. Still, we shouldn't be worrying
+                 ;; about details like jeromq vs jzmq here.
+                 [com.jimrthy/cljeromq "0.1.0-SNAPSHOT" :exclusions [com.stuartsierra/component
+                                                                     org.clojure/clojure
+                                                                     prismatic/schema]]
+                 [com.stuartsierra/component "0.2.3"]
+                 [com.taoensso/timbre "3.4.0" :exclusions [com.taoensso/encore
+                                                           org.clojure/tools.reader]]
                  [im.chit/ribol "0.4.0"]
+                 [io.aviso/config "0.1.1" :exclusions [org.clojure/clojure
+                                                       prismatic/schema]]
                  [mvxcvi/puget "0.8.1"]
                  [org.clojure/clojure "1.7.0-RC1"]
-                 [prismatic/schema "0.4.2"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.reader "0.9.2"]
+                 #_[org.zeromq/cljzmq "0.1.4"]
+                 [prismatic/plumbing "0.4.4"]
+                 [prismatic/schema "0.4.3"]]
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [org.clojure/java.classpath "0.2.2"]]}
+                   :dependencies [[com.jimrthy/component-dsl "0.1.0"]]
+                   :plugins [[org.clojure/tools.namespace "0.2.10"]
+                             [org.clojure/java.classpath "0.2.2"]]}
              :uberjar {:aot :all}}
   :repl-options {:init-ns user})
