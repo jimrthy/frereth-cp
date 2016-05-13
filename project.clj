92801924f092d61b99f0ac7410d237597dae9b4b
James Gatannah <james.gatannah@gmail.com>
2016-05-12 23:51:20 -0500

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -3,16 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;; TODO: Could I totally pull datomic dependencies out of everything else?
-  :dependencies [[com.stuartsierra/component "0.2.3"]
-                 [com.taoensso/timbre "3.4.0" :exclusions [org.clojure/tools.reader]]
-                 [im.chit/ribol "0.4.0"]
-                 [mvxcvi/puget "0.8.1"]
-                 [org.clojure/clojure "1.7.0-RC1"]
-                 [prismatic/schema "0.4.2"]]
+  ;; Q: Could I totally pull datomic dependencies out of everything else
+  ;; by putting them in here?
+  ;; TODO: Pick a date library and use it.
+  :dependencies [;; TODO: Switch to something like component-dsl
+                 [com.stuartsierra/component "0.3.1"]
+                 [com.taoensso/timbre "4.3.1" :exclusions [org.clojure/tools.reader]]
+                 ;; TODO: Switch to hara-events
+                 [im.chit/ribol "0.4.1"]
+                 [mvxcvi/puget "1.0.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [prismatic/schema "1.1.1"]]
 
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [org.clojure/java.classpath "0.2.2"]]}
+                                  [org.clojure/java.classpath "0.2.3"]]}
              :uberjar {:aot :all}}
   :repl-options {:init-ns user})
