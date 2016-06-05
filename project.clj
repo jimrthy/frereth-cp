abb09df56e47039055e5a15b43539feb426b1ee3
James Gatannah <james.gatannah@gmail.com>
2016-06-05 11:16:15 -0500

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -9,11 +9,13 @@
   ;; really shouldn't have access to that sort of thing.
   ;; TODO: Pick a date library and use it.
   :dependencies [;; Q: Do I really want to choose this over clj-time?
-                 [clojure.joda-time "0.6.0"]
+                 ;; A: No.
+                 ;; Better Q: Why is this mentioned here at all?
+                 [clojure.joda-time "0.7.0"]
                  ;; Q: Does this make any sense in production?
                  ;; A: Well, it makes sense for the general runtime which
                  ;; is the primary goal.
-                 [com.cemerick/pomegranate "0.3.0" :exclusions [org.codehaus.plexus/plexus-utils]]
+                 [com.cemerick/pomegranate "0.3.1" :exclusions [org.codehaus.plexus/plexus-utils]]
                  ;; For now, this next library needs to be distributed to
                  ;; a local maven repo.
                  ;; It seems like it should really take care of its handler
@@ -38,15 +40,15 @@
                  [com.jimrthy/component-dsl "0.1.1-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [com.taoensso/timbre "4.3.1" :exclusions [org.clojure/clojure
                                                            org.clojure/tools.reader]]
-                 [com.taoensso/timbre "4.1.4" :exclusions [org.clojure/clojure
+                 [com.taoensso/timbre "4.3.1" :exclusions [org.clojure/clojure
                                                            org.clojure/tools.reader]]
-                 [fullcontact/full.async "0.8.18" :exclusions [org.clojure/clojure
-                                                               org.clojure/core.async]]
+                 [fullcontact/full.async "0.9.0" :exclusions [org.clojure/clojure
+                                                              org.clojure/core.async]]
                  ;; This has been deprecated.
                  ;; TODO: Switch to hara-events
                  [im.chit/ribol "0.4.1" :exclusions [org.clojure/clojure]]
-                 [io.aviso/config "0.1.9" :exclusions [org.clojure/clojure
-                                                       prismatic/schema]]
+                 [io.aviso/config "0.1.13" :exclusions [org.clojure/clojure
+                                                        prismatic/schema]]
                  ;; This is screwing up EDN serialization
                  ;; In particular dates.
                  ;; TODO: Make it ignore those
@@ -55,7 +57,7 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.reader "0.10.0" :exclusions [org.clojure/clojure]]
-                 [prismatic/plumbing "0.5.2"]
+                 [prismatic/plumbing "0.5.3"]
                  [prismatic/schema "1.1.1"]]
   :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:" (System/getenv "LD_LIBRARY_PATH"))]
   :plugins []
