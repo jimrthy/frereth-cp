df07d3a194f782bd5a3a7c2a754fe5290ebfbf54
James Gatannah <james.gatannah@gmail.com>
2016-08-13 06:02:57 +0000

    Require event-loop creator to supply 0mq context
    That part needs to come from a completely differet Component

--- src/com/frereth/common/system.clj
+++ src/com/frereth/common/system.clj
@@ -40,14 +40,10 @@ It seems to make less sense under the system namespace, but
 I'm not sure which alternatives make more sense."
   [{:keys [client-keys
            context
-           ctx-thread-count  ; obsolete
-           direction         ; obsolete
            event-loop-name
            server-key
-           socket-type       ; obsolete
            url]
-    :or {ctx-thread-count 1
-         socket-type :dealer
+    :or {socket-type :dealer
          direction :connect}}]
   (let [url (cond-> url
               (not (:protocol url)) (assoc :protocol :tcp)
@@ -56,21 +52,15 @@ I'm not sure which alternatives make more sense."
     (let [defaults {:event-loop {:_name event-loop-name}
                     :ex-sock {:url url
                               :direction direction
-                              :sock-type socket-type}
-                    :zmq-context {:thread-count ctx-thread-count}}
+                              :sock-type socket-type
+                              :ctx context}}
           description {:structure '{:event-loop com.frereth.common.async-zmq/ctor
                                     :evt-iface com.frereth.common.async-zmq/ctor-interface
                                     :ex-chan com.frereth.common.async-component/chan-ctor
                                     :ex-sock com.frereth.common.zmq-socket/ctor
                                     :in-chan com.frereth.common.async-component/chan-ctor
-                                    :status-chan com.frereth.common.async-component/chan-ctor
-                                    :zmq-context com.frereth.common.zmq-socket/ctx-ctor}
+                                    :status-chan com.frereth.common.async-component/chan-ctor}
                        :dependencies {:evt-iface [:ex-sock :in-chan :status-chan]
                                       :event-loop {:interface :evt-iface
-                                                   :ex-chan :ex-chan}
-                                      :ex-sock {:ctx :zmq-context}}}
-          default-result (cpt-dsl/build description defaults)]
-      (if context
-        ;; Generally, callers will override
-        (assoc default-result :zmq-context context)
-        default-result))))
+                                                   :ex-chan :ex-chan}}}
+          default-result (cpt-dsl/build description defaults)])))
