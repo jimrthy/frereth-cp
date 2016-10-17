f612358410b2f9a35bd9389c1ee0923c71fe1093
James Gatannah <jamesgatannah@gmail.com>
2016-10-17 03:18:02 +0000

    Narrow down a problem w/ Server startup
    Not necessarily the problem, and almost definitely not the really
    interesting part of it.
    
    But it really looks as though my socket constructor is getting called
    with both namespaced and non-namespace keywords. Which almost definitely
    violates the spec.
    
    Not sure how that bleeds over into the generator, but it's the most
    likely candidate I've found so far.

--- src/com/frereth/common/system.clj
+++ src/com/frereth/common/system.clj
@@ -51,10 +51,14 @@ I'm not sure which alternatives make more sense."
     :or {direction :connect
          socket-type :dealer
          thread-count 2}}]
+  (throw (ex-info "Start here"
+                  {:huh? "Compare `git diff`"
+                   :suspicion "Does including non-namespaced keywords royally break (s/explain-data)?"
+                   :otoh "This breaks lots of existing unit tests"}))
   (let [url (cond-> url
-              (not (:protocol url)) (assoc :protocol :tcp)
-              (not (:address url)) (assoc :address [127 0 0 1])
-              (not (:port url)) (assoc :port 9182))]
+              (not (:cljeromq.common/zmq-protocol url)) (assoc :cljeromq.common/zmq-protocol :tcp)
+              (not (:cljeromq.common/zmq-address url)) (assoc :cljeromq.common/zmq-address [127 0 0 1])
+              (not (:cljeromq.common/port url)) (assoc :cljeromq.common/port 9182))]
     (let [options {::context {:thread-count thread-count}
                    ::event-loop {:_name event-loop-name}
                    ::ex-sock {:url url
