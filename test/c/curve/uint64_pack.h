c8058941b1ab4442590b5b768638279396d5d883
James Gatannah <james.gatannah@gmail.com>
2017-04-19 11:31:08 +0000

    Add reference uint64[un]pack from NaCl
    Did modify the files by adding the public domain license at the top.
    
    Planning to use these to write a test program or two to validate exactly
    what I should be producing/consuming.

---
+++ test/c/curve/uint64_pack.h
@@ -0,0 +1,13 @@
+/*
+ * measure-anything.c version 20090223
+ * D. J. Bernstein
+ * Public domain.
+ */
+#ifndef UINT64_PACK_H
+#define UINT64_PACK_H
+
+#include "crypto_uint64.h"
+
+extern void uint64_pack(unsigned char *,crypto_uint64);
+
+#endif
