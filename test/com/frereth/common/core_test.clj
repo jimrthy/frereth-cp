78600dd7b25ba4484c5b3b2371cedc3ca4773495
James Gatannah <james.gatannah@gmail.com>
2017-05-14 15:27:40 +0000

    Remove some unit tests left over from common fork
    They were pieces that just no longer make sense here.

--- test/com/frereth/common/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.frereth.common.core-test
-  (:require [clojure.test :refer :all]
-            [com.frereth.common.core :refer :all]))
-
-(deftest a-test
-  (testing "Reality works"
-    (is (= 1 1))))
