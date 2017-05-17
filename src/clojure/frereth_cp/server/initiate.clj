d429e740a1b6255db70d003a6ece2921b3793402
James Gatannah <james.gatannah@gmail.com>
2017-05-17 04:50:48 +0000

    Fix the key-triad extraction problem
    Debugging this has been the focus of the past few commits.
    
    Root cause was that I was building the shared secred from an array of
    zeros just before I copied the client's public long-term key into that
    array.

--- src/clojure/frereth_cp/server/initiate.clj
+++ src/clojure/frereth_cp/server/initiate.clj
@@ -315,46 +315,48 @@ Note that that includes TODOs re:
 "
   [state supplied-client-short-key client-message-box]
   (let [client-long-buffer (::K/long-term-public-key client-message-box)
-        client-long-key (byte-array K/key-length)
-        my-long-secret (.getSecretKey (get-in state [::shared/my-keys ::shared/long-pair]))
-        shared-secret (crypto/box-prepare client-long-key
-                                          my-long-secret)]
+        client-long-key (byte-array K/key-length)]
     (.getBytes client-long-buffer 0 client-long-key)
-    (log/info (str "Getting ready to decrypt the inner-most hidden public key\n"
-                   "Supplied client long-term key:\n"
-                   ;; This next should match:
-                   #_[0x26 0x9f 0x81 0x8d
-                    0x4a 0xfc 0x55 0x04
-                    0xad 0x01 0x34 0xfd
-                    0xb0 0xa4 0xf4 0x12
-                    0xe3 0x64 0x96 0xae
-                    0x8c 0xe7 0x14 0xb5
-                    0x70 0x76 0x62 0xb2
-                    0x64 0xb8 0x3f 0x19]
-                   (b-t/->string client-long-key)
-                   "My long-term secret key:\n"
-                   (b-t/->string my-long-secret)
-                   "My long-term public key:\n"
-                   (b-t/->string (.getPublicKey (get-in state [::shared/my-keys ::shared/long-pair])))
-                   "Shared:\n"
-                   ;; Client's version:
-                   #_[0xc5 0x91 0x62 0xcd
-                    0xfe 0xc1 0x33 0x89
-                    0xa3 0xff 0x82 0x3d
-                    0x26 0x2d 0x09 0x03
-                    0x09 0xbc 0x9e 0xdd
-                    0x35 0x87 0x1b 0x1c
-                    0x81 0xb5 0xff 0x2a
-                    0xa9 0xe1 0x6b 0x58]
-                   (b-t/->string shared-secret)))
-    ;; Failing here.
-    ;; The shared secret doesn't match what the client used to encrypt the box.
-    (when-let [inner-pk (crypto/open-crypto-box
-                         K/vouch-nonce-prefix
-                         (::K/inner-i-nonce client-message-box)
-                         (::K/hidden-client-short-pk client-message-box)
-                         shared-secret)]
-      (b-t/bytes= supplied-client-short-key inner-pk))))
+    (let [my-long-secret (.getSecretKey (get-in state [::shared/my-keys ::shared/long-pair]))
+          shared-secret (crypto/box-prepare client-long-key
+                                            my-long-secret)]
+      (log/info (str "Getting ready to decrypt the inner-most hidden public key\n"
+                     "Supplied client long-term key:\n"
+                     ;; This next should match:
+                     #_[0x26 0x9f 0x81 0x8d
+                        0x4a 0xfc 0x55 0x04
+                        0xad 0x01 0x34 0xfd
+                        0xb0 0xa4 0xf4 0x12
+                        0xe3 0x64 0x96 0xae
+                        0x8c 0xe7 0x14 0xb5
+                        0x70 0x76 0x62 0xb2
+                        0x64 0xb8 0x3f 0x19]
+                     (b-t/->string client-long-key)
+                     "which arrived in: "
+                     client-long-buffer
+                     "\nMy long-term secret key:\n"
+                     (b-t/->string my-long-secret)
+                     "My long-term public key:\n"
+                     (b-t/->string (.getPublicKey (get-in state [::shared/my-keys ::shared/long-pair])))
+                     "Shared:\n"
+                     ;; Client's version:
+                     #_[0xc5 0x91 0x62 0xcd
+                        0xfe 0xc1 0x33 0x89
+                        0xa3 0xff 0x82 0x3d
+                        0x26 0x2d 0x09 0x03
+                        0x09 0xbc 0x9e 0xdd
+                        0x35 0x87 0x1b 0x1c
+                        0x81 0xb5 0xff 0x2a
+                        0xa9 0xe1 0x6b 0x58]
+                     (b-t/->string shared-secret)))
+      ;; Failing here.
+      ;; The shared secret doesn't match what the client used to encrypt the box.
+      (when-let [inner-pk (crypto/open-crypto-box
+                           K/vouch-nonce-prefix
+                           (::K/inner-i-nonce client-message-box)
+                           (::K/hidden-client-short-pk client-message-box)
+                           shared-secret)]
+        (b-t/bytes= supplied-client-short-key inner-pk)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
