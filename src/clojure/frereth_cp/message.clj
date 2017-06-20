935832f17a6838716e597cc53b72bacb857c7c4f
James Gatannah <james.gatannah@gmail.com>
2017-06-20 04:10:59 +0000

    Add more notes/analysis re: curvecpmessage.c
    Think I have it narrowed down to the "real" Chicago algorithm. As
    opposed to all the pieces-parts that are for shuffling the bits between
    parent and child.

--- src/clojure/frereth_cp/message.clj
+++ src/clojure/frereth_cp/message.clj
@@ -65,6 +65,14 @@ congestion control algorithm")
 ;;;        It's in the middle of a do {} while(0) loop
 ;;;      382-406:  Build (and send) the message packet
 ;;;                N.B.: Ignores most of the ACK bits
+;;;                And really does not seem to match the spec
+;;;                This puts the data block size at pos 46
+;;;                And the data block's first byte  position
+;;;                goes in position 48.
+;;;                The trick happens in line 404: he starts
+;;;                the write to FD9 at offset +7, which is the
+;;;                len/16 byte.
+;;;                So everything else is shifted right by 8 bytes
 ;;;      408: earliestblocktime_compute()
 
 ;;;  411-435: try receiving messages: (DJB)
@@ -75,9 +83,50 @@ congestion control algorithm")
 ;;;  436-614: try processing a message: (DJB)
 ;;;           Lots of code in here.
 ;;;           This seems like the interesting part
+;;;           440: sets maxblocklen=1024
+;;;                Q: Why was it ever 512?
+;;;                Guess: for initial Message part of Initiate packet
+;;;          444-609: handle this message if it's comprehensible: (DJB)
+;;;          610-614: counters/looping
 
 ;;;  615-632: try sending data to child: (DJB)
 ;;;           Big picture: copy data from receivebuf to the child[1] pipe
 ;;;           Then zero the receivevalid flags
 ;;;  634-643: try closing pipe to child: (DJB)
 ;;;           Well, maybe. If we're done with it
+
+;;; 444-609 handle this message if it's comprehensible: (DJB)
+;;; (in more depth)
+;;;         445-450: boilerplate
+;;;         452-453: short-circuiting
+;;;         455: extract ID
+;;;         456-542: loop over range(blocknum)
+;;;                  This looks like the "real" Chicago algorithm
+;;;         544-560: Call acknowledged() several times
+;;;                  This is for several byte ranges in the incoming message
+;;;                  0 to the 8-byte offset starting at byte 8
+;;;                  Then we have pairs of start/stop offsets
+;;;                  where each start is from the previous stop
+;;;                  This seems a bit tedious/inefficient, esp. since
+;;;                  lines 396-401 seem to ignore everything except
+;;;                  the blockid[pos] at position 8 (aka 0) and then
+;;;                  positions 46 and 48.
+;;;                  Those are really the length at position 38 and initial
+;;;                  offset at position 40, thanks to starting the send
+;;;                  with length/16 at offset 7.
+;;;        562-574: calculate start/stop bytes
+;;;        576-579: SF (StopFlag? deals w/ EOF)
+;;;        581-588: copy incoming into receivebuf
+;;;                 set the receivevalid flag
+;;;        589-593: increment the receivebytes count
+;;;        595: never acknowledge a pure acknowledgment (DJB)
+;;;             This short-circuits if something extracts to 0
+;;;             (looks like this is the first of the message structure,
+;;;             which would be the message ID)
+;;;        597: /* XXX: delay acknowledgments */ (DJB)
+;;;        598-608: Looks like this just ACKs everything
+;;;                 Evidence:
+;;;        606: /* XXX: incorporate selective acknowledgents */ (DJB)
+
+;;; 456-542: Loop over (range blocknum)
+;;; (getting into details)
