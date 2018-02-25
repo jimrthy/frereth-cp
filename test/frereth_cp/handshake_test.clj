a9e2ed99e79ee76963a6e8207e85eabc27fe0359
James Gatannah <james.gatannah@gmail.com>
2018-02-25 12:05:56 -0600

    Fix cookie composition unit test (again?)

--- test/frereth_cp/handshake_test.clj
+++ test/frereth_cp/handshake_test.clj
@@ -7,29 +7,36 @@
   (:import io.netty.buffer.Unpooled))
 
 (deftest test-cookie-composition
-  (let [client-extension (byte-array (take 16 (repeat 0)))
-        server-extension (byte-array (take 16 (range)))
-        working-nonce (byte-array (take 24 (drop 40 (range))))
-        boxed (byte-array 200)
-        dst (Unpooled/buffer 400)
-        to-encode {::K/header K/cookie-header
-                   ::K/client-extension client-extension
-                   ::K/server-extension server-extension
-                   ::K/client-nonce-suffix (Unpooled/wrappedBuffer working-nonce
-                                                                   K/server-nonce-prefix-length
-                                                                   K/server-nonce-suffix-length)
-                   ;; This is also a great big FAIL:
-                   ;; Have to drop the first 16 bytes
-                   ;; Q: Have I fixed that yet?
-                   ::K/cookie (Unpooled/wrappedBuffer boxed
-                                                      K/box-zero-bytes
-                                                      144)}]
-    (try
-      ;; FIXME: This fails because we can't cast a ByteBuf to a B]
-      (let [composed (shared/compose K/cookie-frame to-encode dst)]
-        (is composed))
-      (catch clojure.lang.ExceptionInfo ex
-        (is (not (.getData ex)))))))
+  ;; The different magic numbers I'm using for the extensions and
+  ;; nonce are just so I have a way to distinguish the different bits
+  ;; when I need to look at the individual bytes
+  (let [client-extension (byte-array (take K/extension-length (repeat 0)))
+        server-extension (byte-array (take K/extension-length (range)))
+        working-nonce (byte-array (take K/nonce-length (drop 40 (range))))
+        boxed (byte-array K/cookie-packet-length)
+        dst (Unpooled/buffer (* 2 K/cookie-packet-length))
+        client-nonce-suffix-buffer (Unpooled/wrappedBuffer working-nonce
+                                                           K/server-nonce-prefix-length
+                                                           K/server-nonce-suffix-length)
+        client-nonce-suffix (byte-array K/server-nonce-suffix-length)
+        cookie-buffer (Unpooled/wrappedBuffer boxed
+                                              K/box-zero-bytes
+                                              K/cookie-frame-length)
+        cookie (byte-array K/cookie-frame-length)]
+    (.readBytes client-nonce-suffix-buffer client-nonce-suffix)
+    (let [to-encode {::K/header K/cookie-header
+                     ::K/client-extension client-extension
+                     ::K/server-extension server-extension
+                     ::K/client-nonce-suffix client-nonce-suffix
+                     ;; This is also a great big FAIL:
+                     ;; Have to drop the first 16 bytes
+                     ;; Q: Have I fixed that yet?
+                     ::K/cookie cookie}]
+      (try
+        (let [composed (shared/compose K/cookie-frame to-encode dst)]
+          (is composed))
+        (catch clojure.lang.ExceptionInfo ex
+          (is (not (.getData ex))))))))
 
 (deftest vouch-extraction
   ;; TODO:
