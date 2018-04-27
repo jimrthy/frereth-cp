8b6a4ca292235fb05b5f652ed2a5b3e498410c09
James Gatannah <james.gatannah@gmail.com>
2018-04-26 22:00:40 -0500

    Rename log2 alias in cookie to log
    Since it no longer uses clojure.tools.logging.

--- src/clojure/frereth_cp/client/cookie.clj
+++ src/clojure/frereth_cp/client/cookie.clj
@@ -7,8 +7,7 @@
             [frereth-cp.shared.bit-twiddling :as b-t]
             [frereth-cp.shared.constants :as K]
             [frereth-cp.shared.crypto :as crypto]
-            ;; FIXME: Refactor this to plain log
-            [frereth-cp.shared.logging :as log2]
+            [frereth-cp.shared.logging :as log]
             [frereth-cp.shared.specs :as specs]
             [frereth-cp.shared.serialization :as serial]
             [frereth-cp.util :as utils]
@@ -32,7 +31,7 @@
            ::shared/work-area
            ::state/server-security
            ::state/shared-secrets]
-    log-state ::log2/state
+    log-state ::log/state
     :as this}
    {:keys [::K/header
            ::K/client-extension
@@ -40,48 +39,48 @@
     ^bytes client-nonce-suffix ::K/client-nonce-suffix
     ^bytes cookie ::K/cookie
     :as rcvd}]
-  (let [log-state (log2/info log-state
-                             ::decrypt-actual-cookie
-                             "Getting ready to try to extract cookie"
-                             {::raw-cookie cookie
-                              ::human-readable (shared/bytes->string cookie)})
+  (let [log-state (log/info log-state
+                            ::decrypt-actual-cookie
+                            "Getting ready to try to extract cookie"
+                            {::raw-cookie cookie
+                             ::human-readable (shared/bytes->string cookie)})
         {^bytes text ::shared/text
          ^bytes working-nonce ::shared/working-nonce} work-area]
     (assert working-nonce (str "Missing nonce buffer amongst\n"
                                (keys work-area)
                                "\nin\n"
                                (keys this)))
-    (let [log-state (log2/info log-state
-                               ::decrypt-actual-cookie
-                               "Copying nonce prefix"
-                               {::src K/cookie-nonce-prefix
-                                ::dst working-nonce})]
+    (let [log-state (log/info log-state
+                              ::decrypt-actual-cookie
+                              "Copying nonce prefix"
+                              {::src K/cookie-nonce-prefix
+                               ::dst working-nonce})]
       (b-t/byte-copy! working-nonce K/cookie-nonce-prefix)
       (b-t/byte-copy! working-nonce
                       K/server-nonce-prefix-length
                       K/server-nonce-suffix-length
                       client-nonce-suffix)
-      (let [log-state (log2/info log-state
-                                 ::decrypt-actual-cookie
-                                 "Copying encrypted cookie"
-                                 {::target text
-                                  ::this this
-                                  ::my-keys (keys this)})]
+      (let [log-state (log/info log-state
+                                ::decrypt-actual-cookie
+                                "Copying encrypted cookie"
+                                {::target text
+                                 ::this this
+                                 ::my-keys (keys this)})]
         (b-t/byte-copy! text 0 K/cookie-frame-length cookie)
         (let [shared (::state/client-short<->server-long shared-secrets)]
           (when-not shared
             (throw (ex-info "Missing client-short<->server-long secret"
                             {::state/shared-secrets shared-secrets})))
-          (let [log-state (log2/info log-state
-                                     ::decrypt-actual-cookie
-                                     "Trying to decrypt"
-                                     {::shared/text  (b-t/->string text)
-                                      ::shared/working-nonce (b-t/->string working-nonce)
-                                      ::client-short<->server-long (b-t/->string shared)})]
+          (let [log-state (log/info log-state
+                                    ::decrypt-actual-cookie
+                                    "Trying to decrypt"
+                                    {::shared/text  (b-t/->string text)
+                                     ::shared/working-nonce (b-t/->string working-nonce)
+                                     ::client-short<->server-long (b-t/->string shared)})]
             ;; TODO: If/when an exception is thrown here, it would be nice
             ;; to notify callers immediately
             (try
-              (let [{log-state ::log2/state
+              (let [{log-state ::log/state
                      decrypted ::crypto/unboxed} (crypto/open-after log-state text 0 144 working-nonce shared)
                     {server-short-pk ::K/s'
                      server-cookie ::K/black-box
@@ -91,20 +90,20 @@
                                            ::server-cookie server-cookie)]
                 (assoc this
                        ::state/server-security server-security
-                       ::log2/state log-state))
+                       ::log/state log-state))
               (catch ExceptionInfo ex
                 (assoc this
-                       ::log2/state (log2/exception log-state
-                                                    ex
-                                                    ::decrypt-actual-cookie
-                                                    "Decryption failed"
-                                                    (.getData ex)))))))))))
+                       ::log/state (log/exception log-state
+                                                  ex
+                                                  ::decrypt-actual-cookie
+                                                  "Decryption failed"
+                                                  (.getData ex)))))))))))
 
 (defn decrypt-cookie-packet
   [{:keys [::shared/extension
            ::shared/packet
            ::state/server-extension]
-    log-state ::log2/state
+    log-state ::log/state
     :as this}]
   (when-not (= (count packet) K/cookie-packet-length)
     (let [err {::expected-length K/cookie-packet-length
@@ -113,20 +112,20 @@
                ;; Because the stack trace hides
                ::where 'shared.curve.client/decrypt-cookie-packet}]
       (throw (ex-info "Incoming cookie packet illegal" err))))
-  (let [log-state (log2/debug log-state
-                              ::decrypt-cookie-packet
-                              "Incoming packet that looks like it might be a cookie"
-                              {::raw-packet packet
-                               ::human-readable (shared/bytes->string packet)})
+  (let [log-state (log/debug log-state
+                             ::decrypt-cookie-packet
+                             "Incoming packet that looks like it might be a cookie"
+                             {::raw-packet packet
+                              ::human-readable (shared/bytes->string packet)})
         {:keys [::K/header
                 ::K/client-extension
                 ::K/server-extension]
          :as rcvd} (serial/decompose-array K/cookie-frame packet)
-        log-state (log2/info log-state
-                             ::decrypt-cookie-packet
-                             "Verifying that decrypted packet looks like a Cookie"
-                             {::raw-header header
-                              ::human-readable (shared/bytes->string header)})]
+        log-state (log/info log-state
+                            ::decrypt-cookie-packet
+                            "Verifying that decrypted packet looks like a Cookie"
+                            {::raw-header header
+                             ::human-readable (shared/bytes->string header)})]
     ;; Q: How accurate/useful is this approach?
     ;; (i.e. mostly comparing byte array hashes)
     ;; A: Not at all.
@@ -140,7 +139,7 @@
                (b-t/bytes= extension client-extension)
                (b-t/bytes= server-extension server-extension))
       (decrypt-actual-cookie (assoc this
-                                    ::log2/state log-state)
+                                    ::log/state log-state)
                              rcvd))))
 
 (s/fdef received-response
@@ -149,8 +148,8 @@
                      :cookie ::specs/network-packet)
         :ret any?)
 (defn received-response
-  [{log-state ::log2/state
-    :keys [::log2/logger]
+  [{log-state ::log/state
+    :keys [::log/logger]
     :as this}
    notifier
    {:keys [:host :message :port]
@@ -162,10 +161,10 @@
   ;; If they don't match, we need to discard this cookie
   ;; and go back to waiting (don't forget to reduce the
   ;; timeout based on elapsed time)
-  (let [log-state (log2/info log-state
-                             ::received-response
-                             "Possibly got a response from server"
-                             cookie)]
+  (let [log-state (log/info log-state
+                            ::received-response
+                            "Possibly got a response from server"
+                            cookie)]
     (if-not (or (= cookie ::drained)
                 (= cookie ::hello-response-timed-out))
       (if (= K/cookie-packet-length (count message))
@@ -184,16 +183,16 @@
                                                                          ::state/server-extension
                                                                          ::state/server-security
                                                                          ::state/shared-secrets])
-                                                           ::log2/state log-state
+                                                           ::log/state log-state
                                                            ::shared/packet message))]
             (let [this (into this decrypted)
                   {:keys [::shared/my-keys]} this
                   server-short (get-in this
                                        [::state/server-security
                                         ::specs/public-short])
-                  log-state (log2/debug log-state
-                                        ::cookie->vouch
-                                        "Managed to decrypt the cookie")]
+                  log-state (log/debug log-state
+                                       ::cookie->vouch
+                                       "Managed to decrypt the cookie")]
               (assert server-short (str "Missing ::specs/public-short among\n"
                                         (keys (::state/server-security this))
                                         "\namong bigger-picture\n"
@@ -205,55 +204,55 @@
                                    (crypto/box-prepare
                                     server-short
                                     (.getSecretKey my-short-pair)))
-                    log-state (log2/debug log-state
-                                          ::cookie->vouch
-                                          "Prepared shared short-term secret")]
+                    log-state (log/debug log-state
+                                         ::cookie->vouch
+                                         "Prepared shared short-term secret")]
                 (dfrd/success! notifier (assoc this
-                                               ::log2/state log-state
+                                               ::log/state log-state
                                                ::specs/network-packet cookie))))
-            (let [log-state (log2/warn log-state
-                                       ::received-response
-                                       "Unable to decrypt server cookie"
-                                       {::problem cookie})]
-              (dfrd/success! notifier (assoc this ::log2/state log-state))))
+            (let [log-state (log/warn log-state
+                                      ::received-response
+                                      "Unable to decrypt server cookie"
+                                      {::problem cookie})]
+              (dfrd/success! notifier (assoc this ::log/state log-state))))
           ;; TODO: Look into recovering from these
           (catch ExceptionInfo ex
-            (let [log-state (log2/exception log-state
-                                            ex
-                                            ::received-response
-                                            "High-level failure")
-                  log-state (log2/flush-logs! logger log-state)]
-              (dfrd/error! notifier (assoc this ::log2/state log-state))))
+            (let [log-state (log/exception log-state
+                                           ex
+                                           ::received-response
+                                           "High-level failure")
+                  log-state (log/flush-logs! logger log-state)]
+              (dfrd/error! notifier (assoc this ::log/state log-state))))
           (catch RuntimeException ex
-            (let [log-state (log2/exception log-state
-                                            ex
-                                            ::received-response
-                                            "Unexpected failure")
-                  log-state (log2/flush-logs! logger log-state)]
-              (dfrd/error! notifier (assoc this ::log2/state log-state))))
+            (let [log-state (log/exception log-state
+                                           ex
+                                           ::received-response
+                                           "Unexpected failure")
+                  log-state (log/flush-logs! logger log-state)]
+              (dfrd/error! notifier (assoc this ::log/state log-state))))
           (catch Exception ex
-            (let [log-state (log2/exception log-state
-                                            ex
-                                            ::received-response
-                                            "Low-level failure")
-                  log-state (log2/flush-logs! logger log-state)]
-              (dfrd/error! notifier (assoc this ::log2/state log-state))))
+            (let [log-state (log/exception log-state
+                                           ex
+                                           ::received-response
+                                           "Low-level failure")
+                  log-state (log/flush-logs! logger log-state)]
+              (dfrd/error! notifier (assoc this ::log/state log-state))))
           (catch Throwable ex
-            (let [log-state (log2/exception log-state
-                                            ex
-                                            ::received-response
-                                            "Serious Problem")
-                  log-state (log2/flush-logs! logger log-state)]
-              (dfrd/error! notifier (assoc this ::log2/state log-state)))))
-        (let [log-state (log2/warn log-state
-                                   ::received-response
-                                   "Invalid response. Just discard and retry"
-                                   {::problem cookie})]
-          (dfrd/success! notifier (assoc this ::log2/state log-state))))
-      (let [log-state (log2/warn log-state
-                                 ::received-response
-                                 "Server didn't respond to HELLO. Move on to next.")]
-        (dfrd/success! notifier (assoc this ::log2/state log-state))))))
+            (let [log-state (log/exception log-state
+                                           ex
+                                           ::received-response
+                                           "Serious Problem")
+                  log-state (log/flush-logs! logger log-state)]
+              (dfrd/error! notifier (assoc this ::log/state log-state)))))
+        (let [log-state (log/warn log-state
+                                  ::received-response
+                                  "Invalid response. Just discard and retry"
+                                  {::problem cookie})]
+          (dfrd/success! notifier (assoc this ::log/state log-state))))
+      (let [log-state (log/warn log-state
+                                ::received-response
+                                "Server didn't respond to HELLO. Move on to next.")]
+        (dfrd/success! notifier (assoc this ::log/state log-state))))))
 
 (defn hello-response-failed!
   [this failure]
@@ -279,11 +278,11 @@
     (do
       (send wrapper
             update
-            ::log2/state
-            #(log2/info %
-                        ::wait-for-cookie!
-                        "Sent to server"
-                        sent))
+            ::log/state
+            #(log/info %
+                       ::wait-for-cookie!
+                       "Sent to server"
+                       sent))
       (let [chan<-server (::state/chan<-server @wrapper)
             d (strm/try-take! chan<-server
                                 ::drained
