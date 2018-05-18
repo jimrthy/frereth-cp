990f3328f4abcb4b4f08787448644ac691d7989d
James Gatannah <james.gatannah@gmail.com>
2018-05-17 23:05:34 -0500

    Found StackOverflow that's been so much trouble
    This thing has been plaguing me for days. I don't understand why it
    happens, but at least now I can see what triggers the underlying
    problem (I'm trying to call assoc on an agent somewhere...yet another
    reason to just eliminate the thing completely). And the exception isn't
    escaping unhandled.

--- test/frereth_cp/server_test.clj
+++ test/frereth_cp/server_test.clj
@@ -405,7 +405,14 @@
                     cleaned (if (map? client-state)
                               (dissoc client-state ::log/state)
                               {::agent-state client-state})]
-                (println "Stopping client agent" cleaned))
+                (try
+                  (println "Stopping client agent" cleaned)
+                  ;; Here's the StackOverflowError I've been chasing around for so long.
+                  (catch StackOverflowError ex
+                    (is not ex)
+                    (println "Failed to print the cleaned-up client agent state:"
+                             (log/exception-details ex))
+                    (println "The state object that caused problems is a" (class client-state)))))
               (if-let [problem (agent-error client-agent)]
                 (println "Uh-oh. Don't try to stop client-agent. It's in a failed state:\n"
                          problem)
