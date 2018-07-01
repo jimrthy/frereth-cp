de8157d308db873df8de5c54a3505c3d6cf22917
James Gatannah <james.gatannah@gmail.com>
2018-06-30 22:08:02 -0500

    Add cookie-test from several commits back
    I copied a comment test into here several days ago.

---
+++ test/frereth_cp/client/cookie_test.clj
@@ -0,0 +1,14 @@
+(ns frereth-cp.client.cookie-test
+  "Test out pieces involved in client cookie handling"
+  (:require [clojure.test :refer (deftest is testing)]))
+
+
+(deftest url-comparison
+  (testing "Distinct addresses"
+    (let [ca (java.net.InetAddress/getByName "www.google.ca")
+          com (java.net.InetAddress/getByName "www.google.com")]
+      (is (not= ca com))))
+  (testing "Multiple domains at same address"
+    (let [www (java.net.InetAddress/getByName "www.frereth.com")
+          beta (java.net.InetAddress/getByName "beta.frereth.com")]
+      (is (= beta www)))))
